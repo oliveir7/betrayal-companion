070792571b11d22872697f65448da8412aaa81ae
Mateus Oliveira <oliveir7@tcnj.edu>
2017-10-13 12:41:51 -0400

    Added material-ui libary and dependencies.
    Signed-off-by: Mateus Oliveira <oliveir7@tcnj.edu>

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import RaisedButton from 'material-ui/RaisedButton'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h1 className="App-title">Welcome to React</h1>
+                </header>
+                <p className="App-intro">
+                    To get started, edit <code>src/App.js</code> and save to reload.
+                </p>
+                <RaisedButton label='noop'/>
+            </div>
+        </MuiThemeProvider>
     );
   }
 }
