3c04b51e17e2ea8f5da70d8e4c58702b1b1f82bc
Mateus Oliveira <oliveir7@tcnj.edu>
2017-10-14 12:27:38 -0400

    First draft of all character cards loaded to screen.
    Signed-off-by: Mateus Oliveira <oliveir7@tcnj.edu>

--- src/Character.jsx
+++ src/Character.jsx
@@ -3,37 +3,75 @@ import './App.css';
 import Paper from 'material-ui/Paper';
 import { CardHeader } from 'material-ui/Card';
 import WilliamsAvatar from './assets/Darwin Flash Williams.png';
+import HeroData from './assets/HeroData.json';
+import { Card } from 'material-ui/Card';
+import ActionFavorite from 'material-ui/svg-icons/navigation/check';
+import ActionFavoriteBorder from 'material-ui/svg-icons/social/person-add';
+import Checkbox from 'material-ui/Checkbox';
+
+window.id=0;
 
 const bioStyle = {
     fontSize: '10px'
 }
 
-class Character extends Component {
+const boxStyle = {
+    width:30,
+    margin:'0 auto',
+    paddingBottom:'8px'
+}
+
+const HeroCard = ({data, fn}) => {
+    return (
+        <Paper>
+        <Card>
+            <p>{data.Name}</p>
+            <p>{data.Age}</p>
+            <p>{data.Weight}</p>
+            <p>{data.Height}</p>
+            <p>{data.Hobbies}</p>
+            <p>{data.Birthday}</p>
+        </Card>
+        <Checkbox checkedIcon={<ActionFavorite />}
+            uncheckedIcon={<ActionFavoriteBorder />}
+            onCheck={ fn }
+            style={boxStyle} />
+        </Paper>
+    )
+}
+
+
+const HeroList = ({data}) => {
+    const nodes = data.map((HeroData) => {
+        return(<HeroCard key={window.id++} data={HeroData} />);
+    })
+    
+    return(
+        <div style={bioStyle}>{nodes}<br/></div>
+    )
+}
+
+class AllCharacters extends Component {
     
     constructor(props) {
         super(props);
-        this.state = {
-            name: "Flash Williams",
-            desc: "Height: 6'4, Weight: 288 lbs, Birthday: October 18th",
-            btnText: 'Add Player',
-            img: WilliamsAvatar
-        };
+        console.log(props)
+        this.state = {};
+        this.HeroData = HeroData;
+        this.callbackFromParent = props.callbackFromParent;
     }
     
     componentDidMount(){
-        console.log('Character Loaded.');
+        console.log('AllCharacters Loaded.');
     }
         
     render() {
       return (
-         <CardHeader 
-            title={this.state.name} 
-            subtitle={this.state.desc} 
-            avatar={this.state.img} 
-            subtitleStyle={bioStyle}/>
-      );
+          <HeroList data={this.HeroData} fn={this.callbackFromParent}></HeroList>
+      )
+    
     }
     
 }
 
-export default Character;
+export default AllCharacters;
--- src/PlayerSelection.jsx
+++ src/PlayerSelection.jsx
@@ -7,7 +7,7 @@ import { Card } from 'material-ui/Card';
 import Checkbox from 'material-ui/Checkbox';
 import ActionFavorite from 'material-ui/svg-icons/navigation/check';
 import ActionFavoriteBorder from 'material-ui/svg-icons/social/person-add';
-import Character from './Character.jsx';
+import AllCharacters from './Character.jsx';
 
 const style = {
     height: 100,
@@ -21,29 +21,6 @@ const boxStyle = {
     width:30,
     margin:'0 auto',
     paddingBottom:'8px'
-}
-
-
-const HeroList = (props) => {
-  const numbers = props.numbers;
-  const boxChecked = (e, value) => {
-      console.log(e.currentTarget.id);
-  }
-  const listItems = numbers.map((number) =>
-        <Paper style={style} zDepth={1} key={number}>
-            <Card>
-                <Character/>
-            </Card>
-            <Checkbox checkedIcon={<ActionFavorite />}
-                      uncheckedIcon={<ActionFavoriteBorder />}
-                      onCheck={ boxChecked.bind(this) }
-                      style={boxStyle}
-                      id={ 'Hero_' + number } />
-        </Paper>
-    );
-  return (
-    <div>{listItems}</div>
-  );
 }
 
                             
@@ -72,6 +49,7 @@ class PlayerSelection extends Component {
     
     // slight problem with this approach, may need control inside the checkbox to change label
     boxChecked = (event, value) => {
+        console.log('omg amaizn')
         this.updateCount(value)
     }
 
@@ -79,7 +57,7 @@ class PlayerSelection extends Component {
     return (
         <div>
             <p>{this.state.playerCount} Player(s) Selected</p>
-            <HeroList callbackFromParent={this.updateCount} numbers={ [1, 2, 3, 4, 5] }></HeroList>
+            <AllCharacters callbackFromParent={this.boxChecked}/>
         </div>
     );
   }
