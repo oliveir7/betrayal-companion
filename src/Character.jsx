ce9a5dce3f73718b8f66fe91f83e5528bb5d1da8
Mateus Oliveira <oliveir7@tcnj.edu>
2017-10-22 14:59:00 -0400

    You can now select multiple characters which are dynamically added to the screen.
    Signed-off-by: Mateus Oliveira <oliveir7@tcnj.edu>

--- src/Character.jsx
+++ src/Character.jsx
@@ -1,118 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
-import Paper from 'material-ui/Paper';
-//import { CardHeader } from 'material-ui/Card';
 import HeroData from './assets/HeroData.json';
-//import { Card } from 'material-ui/Card';
-//import ActionFavorite from 'material-ui/svg-icons/navigation/check';
-//import ActionFavoriteBorder from 'material-ui/svg-icons/social/person-add';
-//import Checkbox from 'material-ui/Checkbox';
-import RaisedButton from 'material-ui/RaisedButton';
-import Slider from 'rc-slider';
-//import Badge from 'material-ui/Badge';
-import Healing from 'material-ui/svg-icons/image/healing';
-import IconButton from 'material-ui/IconButton';
-import { Grid, Row, Col } from 'react-flexbox-grid';
-//import SelectField from 'material-ui/SelectField';
-//import MenuItem from 'material-ui/MenuItem';
 import Dropdown from './Dropdown.jsx'
 
 window.id=0;
 
-const bioStyle = {
-    fontSize: '0.8em'
-}
-
-//const boxStyle = {
-//    width:30,
-//    margin:'0 auto',
-//    paddingBottom:'8px'
-//}
-
-const imgStyle = {
-    marginTop: '20px',
-    marginLeft: '20px'
-}
-const sliderStyle = {
-    width: '90%',
-    margin: '0 auto',
-    color: '#efefef',
-    fontFamily: "'Nosifer', cursive",
-    fontSize:'1.3em'
-};
-
-const deathIconStyle = {
-    color:'gray',
-    height:15,
-    width:15
-}
-
-const deathStepStyle = {
-    height:15,
-    width:15,
-    padding: 0,
-    margin: 0
-}
-
-const defaultStep = {
-    color:'lightgreen'
-}
-
-function log(value) {
-//  console.log(value); //eslint-disable-line
-}
-
-const bloodtext = {
-    fontFamily: "'Nosifer', cursive"
-}
-
-let BuildSlider = ({statname, statlist, startingindex}) => {
-    let marks = {};
-    statlist.map((item, index) => {
-        if(index === 0){
-            marks[index] = <IconButton style={deathStepStyle} iconStyle={deathIconStyle}><Healing/></IconButton>
-        }else if(index === startingindex){
-            marks[index] = <span style={defaultStep}>{item}</span>
-        } else
-            marks[index] = item;
-    });
-    return (
-        <div style={sliderStyle}>
-            <p>{statname}</p>
-            <Slider min={0} max={statlist.length-1} step={null} marks={marks} defaultValue={startingindex} 
-                trackStyle={[{ backgroundColor: 'darkred' }, { borderColor: 'pink' }]}
-                railStyle={{ backgroundColor: 'black' }}
-                dotStyle={{ backgroundColor: 'black', borderColor:'gray' }}
-                activeDotStyle={{ backgroundColor: 'black',  borderColor:'gray'}}
-                handleStyle={{ backgroundColor: 'darkred',height: 15,width: 15,borderColor:'white' }} />
-            <br/>
-        </div>    
-    )
-}
-
-const HeroCard = ({data, fn, pic}) => {
-
-//    const speedSlider = buildSlider('Speed', data.Speed, data.startingSpeed);
-//    const sanitySlider = buildSlider('Sanity', data.Sanity, data.startingSanity);
-//    const knowledgeSlider = buildSlider('Knowledge', data.Knowledge, data.startingKnowledge);
-    
-    // TODO: this may need to be its own class, i think setting state is the reason why this wont update.
-    // TODO: more visible feedback on the current value of the stat
-    return (
-        <Col xs={12}>
-            <img src={ pic } alt="" height="100" width="100" style={imgStyle}/>
-            <p><strong>{data.Name}</strong></p>
-            <p>Age: {data.Age}, Birthday: {data.Birthday}</p>
-            <p>Weight: {data.Weight}, Height {data.Height}</p>
-            <p>Hobbies: {data.Hobbies}</p>
-            <hr/>
-            <BuildSlider statname='Might' statlist={data.Might} startingindex={data.startingMight-1}/>
-            <BuildSlider statname='Speed' statlist={data.Speed} startingindex={data.startingSpeed-1}/>
-            <BuildSlider statname='Sanity' statlist={data.Sanity} startingindex={data.startingSanity-1}/>
-            <BuildSlider statname='Knowledge' statlist={data.Knowledge} startingindex={data.startingKnowledge-1}/>
-        </Col>
-    )
-}
 
 const importPics = (r) => {
     // dynamically import all images in directory
@@ -125,31 +17,7 @@ const importPics = (r) => {
 let images = importPics(require.context('./assets', false, /\.(png)$/));
 
 
-const HeroList = ({data, fn}) => {
-    
-    const nodes = data.map((HeroData) => {
-        return(
-            <Col xs={2} key={window.id++}>
-                
-                <HeroCard  data={HeroData} fn={fn} pic={images[HeroData.Name + '.png']} />
-            </Col>
-        );
-    });
-    
-    return(
-      <Paper>
-        <Dropdown data={HeroData} images={images}/>
-        <Grid fluid style={bioStyle}>
-            <Row>{nodes}</Row>
-        </Grid>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-      </Paper>
-    )
-}
+
 
 
 class AllCharacters extends Component {
@@ -164,10 +32,7 @@ class AllCharacters extends Component {
         
     render() {
       return (
-          <div>
-            
-            <HeroList data={HeroData} fn={this.callbackFromParent}/>
-          </div>
+            <Dropdown data={HeroData} images={images} />
       )
     }
 }
--- src/Dropdown.jsx
+++ src/Dropdown.jsx
@@ -1,8 +1,133 @@
 import React, { Component } from 'react';
 import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
+import Paper from 'material-ui/Paper';
+import { Grid, Row, Col } from 'react-flexbox-grid';
+import IconButton from 'material-ui/IconButton';
+import Healing from 'material-ui/svg-icons/image/healing';
+import Slider from 'rc-slider';
+import HeroData from './assets/HeroData.json';
 
+const bloodtext = {
+    fontFamily: "'Nosifer', cursive"
+}
 
+const defaultStep = {
+    color:'lightgreen'
+}
+
+const bioStyle = {
+    fontSize: '0.8em'
+}
+
+const imgStyle = {
+    marginTop: '20px',
+    marginLeft: '20px'
+}
+
+const sliderStyle = {
+    width: '90%',
+    margin: '0 auto',
+    color: '#efefef',
+    fontFamily: "'Nosifer', cursive",
+    fontSize:'1.3em'
+};
+
+const deathIconStyle = {
+    color:'gray',
+    height:15,
+    width:15
+}
+
+const deathStepStyle = {
+    height:15,
+    width:15,
+    padding: 0,
+    margin: 0
+}
+
+const dropdownStyle = {
+    width: '80%',
+    marginTop: 30,
+    borderTop: '1px solid #777'
+}
+
+let BuildSlider = ({statname, statlist, startingindex}) => {
+    let marks = {};
+    statlist.map((item, index) => {
+        if(index === 0){
+            marks[index] = <IconButton style={deathStepStyle} iconStyle={deathIconStyle}><Healing/></IconButton>
+        }else if(index === startingindex){
+            marks[index] = <span style={defaultStep}>{item}</span>
+        } else
+            marks[index] = item;
+    });
+    return (
+        <div style={sliderStyle}>
+            <p>{statname}</p>
+            <Slider min={0} max={statlist.length-1} step={null} marks={marks} defaultValue={startingindex} 
+                trackStyle={[{ backgroundColor: 'darkred' }, { borderColor: 'pink' }]}
+                railStyle={{ backgroundColor: 'black' }}
+                dotStyle={{ backgroundColor: 'black', borderColor:'gray' }}
+                activeDotStyle={{ backgroundColor: 'black',  borderColor:'gray'}}
+                handleStyle={{ backgroundColor: 'darkred',height: 15,width: 15,borderColor:'white' }} />
+            <br/>
+        </div>    
+    )
+}
+
+const HeroCard = ({ data , pic, total}) => {
+
+//    const speedSlider = buildSlider('Speed', data.Speed, data.startingSpeed);
+//    const sanitySlider = buildSlider('Sanity', data.Sanity, data.startingSanity);
+//    const knowledgeSlider = buildSlider('Knowledge', data.Knowledge, data.startingKnowledge);
+    
+    // TODO: this may need to be its own class, i think setting state is the reason why this wont update.
+    // TODO: more visible feedback on the current value of the stat
+    return (
+        <Col xs={ (12/total) }>
+            <img src={pic} alt="" height="100" width="100" style={imgStyle}/>
+            <p><strong>{data.Name}</strong></p>
+            <p>Age: {data.Age}, Birthday: {data.Birthday}</p>
+            <p>Weight: {data.Weight}, Height {data.Height}</p>
+            <p>Hobbies: {data.Hobbies}</p>
+            <hr/>
+            <BuildSlider statname='Might' statlist={data.Might} startingindex={data.startingMight-1}/>
+            <BuildSlider statname='Speed' statlist={data.Speed} startingindex={data.startingSpeed-1}/>
+            <BuildSlider statname='Sanity' statlist={data.Sanity} startingindex={data.startingSanity-1}/>
+            <BuildSlider statname='Knowledge' statlist={data.Knowledge} startingindex={data.startingKnowledge-1}/>
+        </Col>
+    )
+}
+
+const ActivePlayers = ({ nameArray, images }) => {
+    
+    if(typeof nameArray === 'undefined' || nameArray.length < 1)
+        return(<div>No Players Selected!</div>);
+    
+    let list = [];
+    let nodes = nameArray.map((name) => {
+        let temp = null;
+        HeroData.map((Hero) => {
+           if(Hero.Name === name){
+               list.push(Hero);
+               temp = (<HeroCard data={Hero} pic={images[Hero.Name + '.png']} total={nameArray.length} key={window.id++} />)
+               return temp;
+           }
+        });
+        
+        console.log(nameArray);
+        console.log(list);
+        console.log(nodes);
+        return temp;
+    });
+    
+    return(
+        <Row>
+            {nodes}
+        </Row>
+    )
+}
 
 class Dropdown extends Component {
     
@@ -12,6 +137,7 @@ class Dropdown extends Component {
         // add logic to check if certain menu items should be disabled..
         this.state = {
             // for each character in HeroData.json, generate a menuItem for the dropdown menu
+            // this needs to be changed...
             data: props.data.map((character) => {
               return(
                 <MenuItem key={window.id++} label={character.Name} value={character.Name}>
@@ -21,6 +147,7 @@ class Dropdown extends Component {
               );
            })
         }
+        this.state.selected = [];
     }
     
     componentDidMount(){
@@ -32,13 +159,25 @@ class Dropdown extends Component {
     };
 
     // event handler for SelectField updates
-    handleChange = (event, index, value) => this.setState({value});
-
+    handleChange = (event, index, value) => {
+        console.log(value);
+        this.setState({value});
+        this.setState({selected : value});
+    }
+    
     render() {
         return (
-          <SelectField value={this.state.value} onChange={this.handleChange}  hintText="Select a character">
+        <Paper>
+            <SelectField multiple={true} value={this.state.value} style={dropdownStyle}
+                        maxHeight={600}
+                        onChange={this.handleChange}  hintText="Select a character">
                 {this.state.data}
           </SelectField>
+            
+            <Grid fluid style={bioStyle}>
+                <ActivePlayers nameArray={this.state.selected} images={this.images}/>
+            </Grid>
+         </Paper>
         );
     }
 }
